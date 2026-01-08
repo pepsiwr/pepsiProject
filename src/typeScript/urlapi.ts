d0043399eb68899f1d9dc284ccab4ee175e87653
Wiwat <wiwadrudchad@gmail.com>
2026-01-08 10:42:10 +0700

    urlapi

--- src/typeScript/urlapi.ts
+++ src/typeScript/urlapi.ts
@@ -1,2 +1,3 @@
-const urlapi = "http://localhost:3000";
+// const urlapi = "http://localhost:3000";
+const urlapi = "https://pepsi-project-2s9h5112u-pepsiwrs-projects.vercel.app";
 export default urlapi;
